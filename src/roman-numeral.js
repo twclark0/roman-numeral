e711a726ce411cf7405f634723d957dff00b8748
twclark0 <tylerc644@gmail.com>
2019-11-04 10:42:31 -0700

    Add additional error case tests; add logic for handling numbers larger than 3999

--- src/roman-numeral.js
+++ src/roman-numeral.js
@@ -1,38 +1,90 @@
 module.exports = val => {
-  // First make sure val is a valid number
-  if (!val || typeof val !== 'number' || isNaN(val)) {
+  // If val is a string, parse to number removing commas
+  let valAsNumber =
+    typeof val === 'string' ? Number(val.replace(/,/g, '')) : val
+
+  // Make sure val is a number, if not return string error message
+  if (
+    !val ||
+    typeof Number(valAsNumber) !== 'number' ||
+    !Number.isInteger(valAsNumber)
+  )
     return 'Please use a valid number'
-  }
 
-  // key shows base Roman numerals and it's negation options
-  const romanKey = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1
-  }
-  // initial romanNumeral string to modify
-  let romanNumeral = ''
-
-  for (let x of Object.keys(romanKey)) {
-    // loop over all 13 options within romanKey
-    while (val >= romanKey[x]) {
-      // while param val is greater than or equal to current romanKey value
-      romanNumeral += x
-      // concat the romanNumeral string with current romanNumeral symbol
-      val -= romanKey[x]
-      // subtract param value with current roman symbol int value
+  const romanConverter = val => {
+    // key shows base Roman numerals and it's negation options
+    const romanKey = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
     }
+    // initial romanNumeral string to modify
+    let romanNumeral = ''
+
+    for (let x of Object.keys(romanKey)) {
+      // loop over all 13 options within romanKey
+      while (val >= romanKey[x]) {
+        // while param val is greater than or equal to current romanKey value
+        romanNumeral += x
+        // concat the romanNumeral string with current romanNumeral symbol
+        val -= romanKey[x]
+        // subtract param value with current roman symbol int value
+      }
+    }
+    return romanNumeral
   }
 
-  return romanNumeral
+  const romanConverterLargeValues = val => {
+    // Final returned string we will modify as we go along
+    let result = ''
+
+    // Current roman numeral int value we will eventually wrap with ()
+    let currentRNValueOfAThousand = ''
+
+    // First while loop handles all multiples of 1000 within a number. For example 2,200,000,000.. will loop twice to handle the the value of 2 trillion and 200 billion values.
+    while (val > 1000) {
+      // initialParen handles concatenating '('
+      let initialParen = ''
+      // lastParen handles concatenating ')'
+      let lastParen = ''
+      // make copy of val param to subtract from later
+      let valCopy = val
+      // power variable determines to what 1000 power the val is, to determine the number of '()'s to wrap
+      let power = 1
+      // inner while loop handles determining number of 1000s current number is made up. For example 4000 is one multiple of 1000, loops one time
+      while (val > 1000) {
+        // mutates val by dividing by 1000 to decrease it's size, affecting the number of times the while loop loops
+        val /= 1000
+        // mutate power to stay consistent with val
+        power *= 1000
+        // mutate initialParen to stay consistent with val
+        initialParen += '('
+        // mutate lastParen to stay consistent with val
+        lastParen += ')'
+      }
+      // Math.floor ensures we only wrap the current value with params. Has issues when numbers are not evenly divisible by 1000
+      currentRNValueOfAThousand = Math.floor(val)
+
+      // mutate val to affect the first while loop val. For example, val is set to 0 on first parent while loop for 4000... val = 4000 - (4 * 1000)
+      val = valCopy - currentRNValueOfAThousand * power
+      // modify result by concatenating total initialParens, the roman numeral for stripped val, and total lastParens
+      result +=
+        initialParen + romanConverter(currentRNValueOfAThousand) + lastParen
+    }
+    return result
+  }
+
+  return valAsNumber > 3999
+    ? romanConverterLargeValues(valAsNumber)
+    : romanConverter(valAsNumber)
 }
--- src/roman-numeral.test.js
+++ src/roman-numeral.test.js
@@ -61,8 +61,20 @@ describe('Duplicating values', () => {
 })
 
 describe('Error case ', () => {
+  it('handles a number as string', () => {
+    expect(romannumeralScript('2')).toBe('II')
+  })
+  it('handles a number as string with commas', () => {
+    expect(romannumeralScript('2,000')).toBe('MM')
+  })
   it('if value passed is zero', () => {
     expect(romannumeralScript(0)).toBe('Please use a valid number')
+  })
+  it('if value passed is a decimal number', () => {
+    expect(romannumeralScript(2.3)).toBe('Please use a valid number')
+  })
+  it('if value passed is a infinity', () => {
+    expect(romannumeralScript(Infinity)).toBe('Please use a valid number')
   })
   it('if value passed is a string', () => {
     expect(romannumeralScript('test')).toBe('Please use a valid number')
@@ -81,5 +93,62 @@ describe('Error case ', () => {
   })
   it('if value passed is an object', () => {
     expect(romannumeralScript({})).toBe('Please use a valid number')
+  })
+})
+
+describe('Larger than 3999 ', () => {
+  describe('individual values', () => {
+    it("returns '(V)' for 5000", () => {
+      expect(romannumeralScript(5000)).toBe('(V)')
+    })
+    it("returns '(X)' for 10000", () => {
+      expect(romannumeralScript(10000)).toBe('(X)')
+    })
+    it("returns '(L)' for 50000", () => {
+      expect(romannumeralScript(50000)).toBe('(L)')
+    })
+    it("returns '(C)' for 100000", () => {
+      expect(romannumeralScript(100000)).toBe('(C)')
+    })
+    it("returns '(D)' for 500000", () => {
+      expect(romannumeralScript(500000)).toBe('(D)')
+    })
+    it("returns '(M)' for 1000000", () => {
+      expect(romannumeralScript(1000000)).toBe('(M)')
+    })
+  })
+  describe(' negating values', () => {
+    it("returns '(IV)' for 4000", () => {
+      expect(romannumeralScript(4000)).toBe('(IV)')
+    })
+    it("returns '(IX)' for 9000", () => {
+      expect(romannumeralScript(9000)).toBe('(IX)')
+    })
+    it("returns '(XL)' for 40000", () => {
+      expect(romannumeralScript(40000)).toBe('(XL)')
+    })
+    it("returns '(XC)' for 90000", () => {
+      expect(romannumeralScript(90000)).toBe('(XC)')
+    })
+    it("returns '(CD)' for 400000", () => {
+      expect(romannumeralScript(400000)).toBe('(CD)')
+    })
+    it("returns '(CM)' for 900000", () => {
+      expect(romannumeralScript(900000)).toBe('(CM)')
+    })
+  })
+  describe('Duplicating values', () => {
+    it("returns '(XX)' for 20000", () => {
+      expect(romannumeralScript(20000)).toBe('(XX)')
+    })
+    it("returns '(CC)' for 200000", () => {
+      expect(romannumeralScript(200000)).toBe('(CC)')
+    })
+    it("returns '((II))' for 2000000", () => {
+      expect(romannumeralScript(2000000)).toBe('((II))')
+    })
+    it("returns '(((II)))((CC))' for 2200000000", () => {
+      expect(romannumeralScript(2200000000)).toBe('(((II)))((CC))')
+    })
   })
 })
